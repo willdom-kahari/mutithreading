fd6a3bf12eb1088b6925fc02fdec2c6191dfc9fd
willdomkahari <developer.wadu@gmail.com>
2025-04-15 20:57:04 +0200

    FEAT: kata 1

---
+++ src/main/java/month1/kata/Kata1.java
@@ -0,0 +1,27 @@
+package month1.kata;
+
+
+
+/**
+ * Create 10 threads, each print 1–100 sequentially (observe interleaving).
+ * @author <a href="mailto:developer.wadu@gmail.com">Willdom Kahari</a>
+ */
+public class Kata1 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            Thread t1 = new Thread(new NumberPrinter());
+            t1.start();
+        }
+    }
+}
+
+class NumberPrinter implements Runnable{
+
+
+    @Override
+    public void run() {
+        for (int i = 1; i < 101; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+        }
+    }
+}
